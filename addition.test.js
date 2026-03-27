3e9949632cebe446f50cb1f55ea1575d33019ee6
Anna Moughnie <anna@moughnie.com>
2026-03-27 14:37:45 -0700

    initial commit

---
+++ addition.test.js
@@ -0,0 +1,19 @@
+// Operation should return sum of two numbers
+
+const { addition } = require("./addition");
+
+test("2 + 4 = 6", () => {
+    expect(addition(2, 4)).toBe(6);
+});
+
+test("19 + 84 = 103", () => {
+    expect(addition(19, 84)).toBe(103);
+});
+
+test.each([
+    [3, 4, 7],
+    [5, -10, 15],
+    [0, 2, 2],
+])("a + b = c", (a, b, expected) => {
+    a + b === c;
+});
